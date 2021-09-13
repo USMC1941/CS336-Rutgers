8c84eb5bb179fd668a13bca544f60cc8ed562730
USMC1941 <16388993+USMC1941@users.noreply.github.com>
2021-09-13 10:52:40 -0400

    Upgrade Gradle. Remove jcenter() as it is deprecated.

--- Project/CS336_SampleProject_Gradle/build.gradle.kts
+++ Project/CS336_SampleProject_Gradle/build.gradle.kts
@@ -1,28 +1,30 @@
 plugins {
-   java
-   war
+    war
 }
 
 group = "com.cs336"
 version = "1.0-SNAPSHOT"
 
 repositories {
-   mavenCentral()
-   jcenter()
+    mavenCentral()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 java {
-   sourceCompatibility = JavaVersion.VERSION_1_8
-   targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
-   // https://search.maven.org/artifact/mysql/mysql-connector-java/8.0.22/jar
-   implementation("mysql:mysql-connector-java:8.0.22")
+    // https://search.maven.org/artifact/mysql/mysql-connector-java
+    implementation("mysql:mysql-connector-java:8.0.26")
 
-   // https://search.maven.org/artifact/javax.servlet.jsp/javax.servlet.jsp-api/2.3.3/jar
-   implementation("javax.servlet.jsp:javax.servlet.jsp-api:2.3.3")
+    // https://search.maven.org/artifact/javax.servlet.jsp/javax.servlet.jsp-api
+    implementation("javax.servlet.jsp:javax.servlet.jsp-api:2.3.3")
 
-   //https://search.maven.org/artifact/javax.servlet/javax.servlet-api/4.0.1/jar
-   implementation("javax.servlet:javax.servlet-api:4.0.1")
+    // https://search.maven.org/artifact/javax.servlet/javax.servlet-api/
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
 }
--- Project/CS336_SampleProject_Gradle/src/main/java/com/cs336/pkg/ApplicationDB.java
+++ Project/CS336_SampleProject_Gradle/src/main/java/com/cs336/pkg/ApplicationDB.java
@@ -6,40 +6,36 @@ import java.sql.SQLException;
 
 public class ApplicationDB {
 
-   public ApplicationDB() {
+    public ApplicationDB() {}
 
-   }
+    public static void main(String[] args) {
+        ApplicationDB applicationDB = new ApplicationDB();
+        Connection connection = applicationDB.getConnection();
 
-   public static void main(String[] args) {
-      ApplicationDB applicationDB = new ApplicationDB();
-      Connection    connection    = applicationDB.getConnection();
+        System.out.println(connection);
+        applicationDB.closeConnection(connection);
+    }
 
-      System.out.println(connection);
-      applicationDB.closeConnection(connection);
-   }
+    public Connection getConnection() {
+        String connectionURL = "jdbc:mysql://localhost:3306/barbeerdrinkersample";
+        Connection connection = null;
 
-   public Connection getConnection() {
-      String     connectionURL = "jdbc:mysql://localhost:3306/barbeerdrinkersample";
-      Connection connection    = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to your DB. "dbUsername" and "dbPassword" are the databases'
+            // username and password respectively.
+            connection = DriverManager.getConnection(connectionURL, "dbUsername", "dbPassword");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
-      try {
-         Class.forName("com.mysql.cj.jdbc.Driver");
-
-         // Create a connection to your DB. "dbUsername" and "dbPassword" are the databases' username and password respectively.
-         connection = DriverManager.getConnection(connectionURL, "dbUsername", "dbPassword");
-      }
-      catch (ClassNotFoundException | SQLException e) {
-         e.printStackTrace();
-      }
-      return connection;
-   }
-
-   public void closeConnection(Connection connection) {
-      try {
-         connection.close();
-      }
-      catch (SQLException e) {
-         e.printStackTrace();
-      }
-   }
+    public void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
